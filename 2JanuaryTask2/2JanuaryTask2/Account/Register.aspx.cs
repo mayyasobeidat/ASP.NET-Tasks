c74fcb87d3997b0f457736ccc5266d65d8ad9337
15 - Mayyas Obeidat <mayyasobeidat@gmail.com>
2023-01-04 10:26:48 +0300

    new task

--- 2JanuaryTask2/2JanuaryTask2/Account/Register.aspx.cs
+++ 2JanuaryTask2/2JanuaryTask2/Account/Register.aspx.cs
@@ -16,38 +16,52 @@ namespace _2JanuaryTask2.Account
     {
         protected void CreateUser_Click(object sender, EventArgs e)
         {
-            //var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
-            //var signInManager = Context.GetOwinContext().Get<ApplicationSignInManager>();
-            //var user = new ApplicationUser() { UserName = Email.Text, Email = Email.Text };
-            //IdentityResult result = manager.Create(user, Password.Text);
-            //if (result.Succeeded)
-            //{
-            //    // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
-            //    //string code = manager.GenerateEmailConfirmationToken(user.Id);
-            //    //string callbackUrl = IdentityHelper.GetUserConfirmationRedirectUrl(code, user.Id, Request);
-            //    //manager.SendEmail(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>.");
+            var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            var signInManager = Context.GetOwinContext().Get<ApplicationSignInManager>();
+            var user = new ApplicationUser() { UserName = Email.Text, Email = Email.Text };
+            IdentityResult result = manager.Create(user, Password.Text);
+            if (result.Succeeded)
+            {
+                string Id = "";
+                SqlConnection conn = new SqlConnection("data source =DESKTOP-HDOBIGI\\SQLEXPRESS01; database = mayyas ; integrated security=SSPI");
+                SqlCommand command1 = new SqlCommand($"select Id from AspNetUsers where email='{Email.Text}'", conn);
+                conn.Open();
+                SqlDataReader sdr = command1.ExecuteReader();
+                if (sdr.Read())
+                {
+                    Id = sdr[0].ToString();
+                }
+                conn.Close();
+                SqlCommand command2 = new SqlCommand($"Insert into AspNetUserRoles values('{Id}',{Convert.ToInt32(RadioButtonList1.SelectedValue)})", conn);
+                conn.Open();
+                command2.ExecuteNonQuery();
+                conn.Close();
+                // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
+                //string code = manager.GenerateEmailConfirmationToken(user.Id);
+                //string callbackUrl = IdentityHelper.GetUserConfirmationRedirectUrl(code, user.Id, Request);
+                //manager.SendEmail(user.Id, "Confirm your account", "Please confirm your account by clicking <a href="" + callbackUrl + "">here</a>.");
 
-            //    signInManager.SignIn(user, isPersistent: false, rememberBrowser: false);
-            //    IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
-            //}
-            //else 
-            //{
-            //    ErrorMessage.Text = result.Errors.FirstOrDefault();
-            //}
+                signInManager.SignIn(user, isPersistent: false, rememberBrowser: false);
+                IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
+            }
+            else
+            {
+                ErrorMessage.Text = result.Errors.FirstOrDefault();
+            }
 
 
 
-            var connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
-            SqlConnection Con = new SqlConnection(connectionString);
+            //var connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            //SqlConnection Con = new SqlConnection(connectionString);
 
-            Con.Open();
-            Guid roles = Guid.NewGuid();
-            SqlCommand command = new SqlCommand($"insert into AspNetUsers values('{roles}','{Email.Text}',0,'AP9PN4vXAiCaMdMW++OumMI0Mfvoq1tGvwlDBwPj4GD95aJNAXrHc1jWc5bJG80uTw ==','AIa5VYrpJy2KqO29U','',0,0,1,1,0,'{Email.Text}')", Con);
-            command.ExecuteNonQuery();
-            string query = $"insert into AspNetUserRoles values('{roles}',{RadioButtonList1.SelectedValue})";
-            SqlCommand rolecommand = new SqlCommand(query, Con);
-            rolecommand.ExecuteNonQuery();
-            Con.Close();
+            //Con.Open();
+            //Guid roles = Guid.NewGuid();
+            //SqlCommand command = new SqlCommand($"insert into AspNetUsers values('{roles}','{Email.Text}',0,'AP9PN4vXAiCaMdMW++OumMI0Mfvoq1tGvwlDBwPj4GD95aJNAXrHc1jWc5bJG80uTw ==','AIa5VYrpJy2KqO29U','',0,0,1,1,0,'{Email.Text}')", Con);
+            //command.ExecuteNonQuery();
+            //string query = $"insert into AspNetUserRoles values('{roles}',{RadioButtonList1.SelectedValue})";
+            //SqlCommand rolecommand = new SqlCommand(query, Con);
+            //rolecommand.ExecuteNonQuery();
+            //Con.Close();
 
 
 
